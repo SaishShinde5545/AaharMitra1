59b9659f089babff23e140c7564ef40f3bfc4fbf
SaishShinde5545 <temp2three@gmail.com>
2025-11-03 03:24:51 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-  host: 'localhost', // or your DB host
-  user: 'root', // your MySQL username 
-  password: 'sakshi', // your MySQL password
- database: 'aahaarmitra' // your database name
+  host: 'sql12.freesqldatabase.com:3360', // or your DB host
+  user: 'sql12805514', // your MySQL username 
+  password: 'kdCtrEXSua', // your MySQL password
+ database: 'sql12805514' // your database name
   });
   connection.connect((err) => {
      if (err) { console.error('❌ Database connection failed:', err.stack);
@@ -11,4 +11,5 @@ const connection = mysql.createConnection({
        }
       console.log('✅ Connected to MySQL database');
      }); 
+
   module.exports = connection;
